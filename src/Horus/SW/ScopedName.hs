9fd1b3b2c92f4d05c170c99d8e0aa107f3b0e2ea
ElijahVlasov <elijah.vlasov@gmail.com>
2022-05-27 13:26:22 +0300

    Added parsing of starkware_types (#3)
    * Added parsing of starkware_types
    
    * type parsing
    
    * refactoring
    
    * Linting changes
    
    * fixed fourmolu stuff
    
    * remove build issues
    
    * fixed pr comments
    
    * refactoring
    
    * removed redundant dependencies, moved to Text

---
+++ src/Horus/SW/ScopedName.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Horus.SW.ScopedName (ScopedName (..), fromText) where
+
+import Data.Aeson
+import Data.Text
+
+newtype ScopedName = ScopedName {sn_Path :: [Text]} deriving (Eq, Ord)
+
+fromText :: Text -> ScopedName
+fromText scope = ScopedName $ splitOn "." scope
+
+instance Show ScopedName where
+  show (ScopedName path) = unpack $ intercalate "." path
+
+instance Semigroup ScopedName where
+  lhs <> rhs = ScopedName (sn_Path lhs ++ sn_Path rhs)
+
+instance FromJSONKey ScopedName where
+  fromJSONKey = FromJSONKeyText $ fromText
+
+instance FromJSON ScopedName where
+  parseJSON = withText "ScopedName" $ \v ->
+    pure $ fromText v
