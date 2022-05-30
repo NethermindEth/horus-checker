e777c88f27aa393bb7129e84c319badee95230f8
Artem Yurchenko <artemyurchenko@zoho.com>
2022-05-30 19:35:24 +0400

    implement ControlFlowGraph building

--- src/Horus/SMTUtil.hs
+++ src/Horus/SMTUtil.hs
@@ -1,9 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
+module Horus.SMTUtil (prime, ap, fp, memory, inferJnzCondition) where
 
-module Horus.SMTUtil (prime, ap, fp, memory) where
-
-import SimpleSMT.Typed (TSExpr)
+import Horus.Instruction (Instruction (..), PointerRegister (..))
+import SimpleSMT.Typed (TSExpr, (./=))
 import qualified SimpleSMT.Typed as SMT
+
+inferJnzCondition :: Instruction -> TSExpr Bool
+inferJnzCondition i = memory dstAddr ./= 0
+ where
+  reg = case i_dstRegister i of
+    AllocationPointer -> ap
+    FramePointer -> fp
+  dstAddr = (reg + fromInteger (i_op0Offset i)) `SMT.mod` prime
 
 prime :: TSExpr Integer
 prime = SMT.const "prime"
