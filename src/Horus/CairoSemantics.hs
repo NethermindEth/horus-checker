01eb39b6a6de857499c2b61ea01aeb872f310336
Ilya Vlasov <elijah.vlasov@gmail.com>
2022-05-30 11:58:59 +0300

    added instruction semantics

---
+++ src/Horus/CairoSemantics.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -w #-}
+
+module Horus.CairoSemantics where
+
+import Control.Lens
+import Control.Lens.TH
+import Control.Monad.State
+
+import Data.Map
+
+import GHC.Num (Integer)
+import Horus.Instruction
+import SimpleSMT.Typed
+
+newtype MemVar = MemVar {unMemVar :: TSExpr Integer}
+
+fieldPrime :: TSExpr Integer
+fieldPrime = int 0x800000000000011000000000000000000000000000000000000000000000001
+
+data ConstraintsState = ConstraintsState
+  { _memoryVariables :: Map MemVar (TSExpr Integer)
+  , _exprs :: [TSExpr Bool]
+  , _decls :: [TSExpr ()]
+  }
+
+$(makeLenses ''ConstraintsState)
+
+addBounds :: TSExpr Integer -> State ConstraintsState ()
+addBounds expr = do
+  state <- get
+  put $ over exprs (int 0 .<= expr :) (over exprs (expr .< fieldPrime :) state)
+
+alloc :: TSExpr Integer -> State ConstraintsState (TSExpr Integer)
+alloc address = do
+  state <- get
+  let nextMemVar = "MEM" <> show (size (_memoryVariables state))
+  put $ over decls (declareInt nextMemVar :) state
+  addBounds $ constInt nextMemVar
+  return $ constInt nextMemVar
+
+type Step = Integer
+
+apStep :: Step -> TSExpr Integer
+apStep step = constInt $ "ap" <> show step
+fpStep :: Step -> TSExpr Integer
+fpStep step = constInt $ "fp" <> show step
+registerStep :: Step -> PointerRegister -> TSExpr Integer
+registerStep step AllocationPointer = apStep step
+registerStep step FramePointer = fpStep step
+
+mkInstructionConstraints :: Instruction -> Step -> State ConstraintsState (TSExpr Bool)
+mkInstructionConstraints
+  ( Instruction
+      dstRegister
+      op0Register
+      op1Source
+      resLogic
+      pcUpdate
+      apUpdate
+      opCode
+      op0Offset
+      op1Offset
+      dstOffset
+      fpUpdate
+      imm
+    )
+  step = do
+    op0 <- alloc (registerStep step op0Register + int op0Offset)
+    op1 <- case op1Source of
+      Op0 -> alloc $ (op0 + int op1Offset) .% fieldPrime
+      RegisterSource reg -> alloc (registerStep step reg + int op1Offset)
+      Imm -> case imm of
+        Just x -> pure $ int x
+        Nothing -> error "Imm has to contain a value"
+    let res = case resLogic of
+          Op1 -> op1 .% fieldPrime
+          Add -> (op0 + op1) .% fieldPrime
+          Mult -> (op0 * op1) .% fieldPrime
+          Unconstrained -> int 0
+    dst <- alloc (registerStep step dstRegister + int dstOffset)
+    let conditionFormula = dst .== int 0
+    let currentAp = registerStep step AllocationPointer
+    let apUpdateFormula =
+          registerStep
+            (step + 1)
+            AllocationPointer
+            .== case apUpdate of
+              NoUpdate -> currentAp
+              AddRes -> (currentAp + res) .% fieldPrime
+              Add1 -> (currentAp + 1) .% fieldPrime
+              Add2 -> (currentAp + 2) .% fieldPrime
+    let currentFp = registerStep step FramePointer
+    let fpUpdateFormula =
+          registerStep
+            (step + 1)
+            FramePointer
+            .== case fpUpdate of
+              KeepFp -> currentFp
+              ApPlus2 -> (currentAp + 2) .% fieldPrime
+              Dst -> dst
+    let instructionAssertion = case opCode of
+          AssertEqual -> res .== dst
+          _ -> bool True
+    return $ conditionFormula .&& apUpdateFormula .&& fpUpdateFormula .&& instructionAssertion
