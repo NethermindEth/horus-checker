597e39eb21c469a6c0837bfda5d1245477fdc75c
Artem Yurchenko <artemyurchenko@zoho.com>
2022-06-02 16:30:02 +0400

    add creation of modules from the CFG
    modules are pieces of a program that can be smt-checked in isolation

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import Data.Text (unpack)
 import System.Environment (getArgs)
 import Text.Pretty.Simple (pPrint)
 
-import Horus.Global (makeCFG)
+import Horus.Global (makeCFG, makeModules)
 import qualified Horus.Global.Runner as Global (run)
 
 main :: IO ()
@@ -14,9 +14,13 @@ main = do
   mbContract <- eitherDecodeFileStrict filename
   case mbContract of
     Left err -> fail err
-    Right contract -> case Global.run (makeCFG contract) of
-      Left err -> fail (unpack err)
-      Right cfg -> pPrint cfg
+    Right contract -> do
+      case Global.run (makeCFG contract) of
+        Left err -> fail (unpack err)
+        Right cfg -> pPrint cfg
+      case Global.run (makeModules contract) of
+        Left err -> fail (unpack err)
+        Right modules -> pPrint modules
 
 parseArgs :: [String] -> IO String
 parseArgs [filename] = pure filename
