a112d7c740637feb933fc1d7630f93f9b9b84611
Artem Yurchenko <artemyurchenko@zoho.com>
2022-07-05 13:05:35 +0400

    use ApTracking data from the compiler, instead of manual updating
    That allows us to get rid of unnecessarily verbose models, with a pair
         of ap and fp variables for each instruction. Furthermore, the
         number of memory variables and memory equations (quadratic in the
         number of memory variables) now has a potential to be reduced, if
         we won't duplicate memory variables with the same address.

--- src/Horus/SMTUtil.hs
+++ src/Horus/SMTUtil.hs
@@ -2,17 +2,13 @@ module Horus.SMTUtil
   ( prime
   , ap
   , fp
+  , regToTSExpr
   , memory
-  , apStep
-  , fpStep
-  , Step
-  , substituteFpAndAp
   )
 where
 
-import Data.Text (Text)
-
-import SimpleSMT.Typed (TSExpr, substitute)
+import Horus.Instruction (PointerRegister (..))
+import SimpleSMT.Typed (TSExpr)
 import qualified SimpleSMT.Typed as SMT
 
 prime :: TSExpr Integer
@@ -22,15 +18,9 @@ ap, fp :: TSExpr Integer
 ap = SMT.const "ap"
 fp = SMT.const "fp"
 
+regToTSExpr :: PointerRegister -> TSExpr Integer
+regToTSExpr AllocationPointer = ap
+regToTSExpr FramePointer = fp
+
 memory :: TSExpr Integer -> TSExpr Integer
 memory = SMT.function "memory"
-
-type Step = Text
-
-apStep :: Step -> TSExpr Integer
-apStep step = SMT.const $ "ap" <> step
-fpStep :: Step -> TSExpr Integer
-fpStep step = SMT.const $ "fp" <> step
-
-substituteFpAndAp :: Step -> TSExpr a -> TSExpr a
-substituteFpAndAp step = substitute "ap" (apStep step) . substitute "fp" (fpStep step)
