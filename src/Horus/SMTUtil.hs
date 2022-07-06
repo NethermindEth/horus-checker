56083e8347a699149cfd8dcbcae6843643a3fd8b
Artem Yurchenko <artemyurchenko@zoho.com>
2022-07-06 20:42:25 +0400

    apply RAII to SMT solver process creation

--- src/Horus/SMTUtil.hs
+++ src/Horus/SMTUtil.hs
@@ -4,10 +4,15 @@ module Horus.SMTUtil
   , fp
   , regToTSExpr
   , memory
+  , withSolver
   )
 where
 
+import Control.Exception.Safe (bracket)
+import Data.Text (Text, unpack)
+
 import Horus.Instruction (PointerRegister (..))
+import qualified SimpleSMT (Solver, newSolver, stop)
 import SimpleSMT.Typed (TSExpr)
 import qualified SimpleSMT.Typed as SMT
 
@@ -24,3 +29,9 @@ regToTSExpr FramePointer = fp
 
 memory :: TSExpr Integer -> TSExpr Integer
 memory = SMT.function "memory"
+
+withSolver :: Text -> [Text] -> (SimpleSMT.Solver -> IO a) -> IO a
+withSolver solverName args =
+  bracket
+    (SimpleSMT.newSolver (unpack solverName) (map unpack args) Nothing)
+    SimpleSMT.stop
