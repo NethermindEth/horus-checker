50192ab60a771f4eafc35479b3a016cfe67f3c3c
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2022-07-14 20:52:37 +0200

    add inlinability analysis, add inlining

--- src/Horus/CFGBuild/Runner.hs
+++ src/Horus/CFGBuild/Runner.hs
@@ -19,7 +19,8 @@ import Lens.Micro.GHC ()
 import Lens.Micro.Mtl ((%=))
 
 import Horus.CFGBuild (ArcCondition (..), CFGBuildF (..), CFGBuildT (..), Label, LabeledInst)
-import Horus.ScopedTSExpr (ScopedTSExpr)
+import Horus.FunctionAnalysis (FInfo)
+import SimpleSMT.Typed (TSExpr)
 
 newtype ImplT m a = ImplT (ExceptT Text (StateT CFG m) a)
   deriving newtype (Functor, Applicative, Monad, MonadState CFG, MonadError Text)
@@ -29,8 +30,8 @@ instance MonadTrans ImplT where
 
 data CFG = CFG
   { cfg_vertices :: [Label]
-  , cfg_arcs :: Map Label [(Label, [LabeledInst], ArcCondition)]
-  , cfg_assertions :: Map Label [ScopedTSExpr Bool]
+  , cfg_arcs :: Map Label [(Label, [LabeledInst], ArcCondition, FInfo)]
+  , cfg_assertions :: Map Label [TSExpr Bool]
   }
   deriving (Show)
 
@@ -40,7 +41,7 @@ emptyCFG = CFG [] Map.empty Map.empty
 cfgVertices :: Lens' CFG [Label]
 cfgVertices lMod g = fmap (\x -> g{cfg_vertices = x}) (lMod (cfg_vertices g))
 
-cfgArcs :: Lens' CFG (Map Label [(Label, [LabeledInst], ArcCondition)])
+cfgArcs :: Lens' CFG (Map Label [(Label, [LabeledInst], ArcCondition, FInfo)])
 cfgArcs lMod g = fmap (\x -> g{cfg_arcs = x}) (lMod (cfg_arcs g))
 
 cfgAssertions :: Lens' CFG (Map Label [ScopedTSExpr Bool])
@@ -50,9 +51,9 @@ interpret :: Monad m => CFGBuildT m a -> ImplT m a
 interpret = iterTM exec . runCFGBuildT
  where
   exec (AddVertex l cont) = cfgVertices %= ([l] `union`) >> cont
-  exec (AddArc lFrom lTo insts test cont) = cfgArcs . at lFrom %= doAdd >> cont
+  exec (AddArc lFrom lTo insts test isF cont) = cfgArcs . at lFrom %= doAdd >> cont
    where
-    doAdd mArcs = Just ((lTo, insts, test) : mArcs ^. _Just)
+    doAdd mArcs = Just ((lTo, insts, test, isF) : mArcs ^. _Just)
   exec (AddAssertion l assertion cont) = cfgAssertions . at l %= doAdd >> cont
    where
     doAdd mAssertions = Just (assertion : mAssertions ^. _Just)
