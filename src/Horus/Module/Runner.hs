f025f92c852e27e9b44e71b36f02a73b949fa43f
Artem Yurchenko <artemyurchenko@zoho.com>
2022-08-17 20:18:48 +0400

    fix: segments from unannotated loops were omitted
    now, unannotated loops cause an error

--- src/Horus/Module/Runner.hs
+++ src/Horus/Module/Runner.hs
@@ -1,19 +1,22 @@
 module Horus.Module.Runner (interpret, run) where
 
+import Control.Monad.Except (ExceptT, catchError, runExceptT, throwError)
 import Control.Monad.Free.Church (iterM)
-import Control.Monad.Reader (Reader, ask, local, runReader)
-import Control.Monad.Writer (WriterT, execWriterT, tell)
+import Control.Monad.Reader (ReaderT, ask, local, runReaderT)
+import Control.Monad.Writer (Writer, runWriter, tell)
 import Data.DList (DList)
 import Data.DList qualified as D (singleton)
 import Data.Foldable (toList)
 import Data.Function ((&))
+import Data.Functor (($>))
 import Data.Set (Set)
 import Data.Set qualified as Set (empty, insert, member)
+import Data.Text (Text)
 
 import Horus.CFGBuild (Label (..))
 import Horus.Module (Module (..), ModuleF (..), ModuleL (..))
 
-type Impl = WriterT (DList Module) (Reader (Set Label))
+type Impl = ReaderT (Set Label) (ExceptT Text (Writer (DList Module)))
 
 interpret :: ModuleL a -> Impl a
 interpret = iterM exec . runModuleL
@@ -24,10 +27,14 @@ interpret = iterM exec . runModuleL
     visited <- ask
     local (Set.insert l) $ do
       interpret (action (Set.member l visited)) >>= cont
+  exec (Throw t) = throwError t
+  exec (Catch m handler cont) = catchError (interpret m) (interpret . handler) >>= cont
 
-run :: ModuleL a -> [Module]
-run m =
-  interpret m
-    & execWriterT
-    & flip runReader Set.empty
-    & toList
+run :: ModuleL a -> Either Text [Module]
+run m = value $> toList output
+ where
+  (value, output) =
+    interpret m
+      & flip runReaderT Set.empty
+      & runExceptT
+      & runWriter
