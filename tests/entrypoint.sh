1b3929dfd837ccfc0fcda9a5a44733b4ea5d0463
Agustín Martinez Suñé <4691852+aemartinez@users.noreply.github.com>
2022-09-20 10:48:08 -0300

    bats not parallel

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -5,5 +5,6 @@ set -eu
 RES_DIR="./tests/resources"
 bash "${RES_DIR}/generate-bats.sh" > "${RES_DIR}/tests.bats"
 jobs=$(getconf _NPROCESSORS_ONLN)
-bats --jobs "${jobs}" "${RES_DIR}/tests.bats"
+# bats --jobs "${jobs}" "${RES_DIR}/tests.bats"
+bats "${RES_DIR}/tests.bats"
 rm "${RES_DIR}/tests.bats"
