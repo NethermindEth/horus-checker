bc6c0046832320cc65e66b96629091f3101781a1
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-11-18 13:06:43 -0500

    Use `sudo` in `mathsat` install scripts.

--- scripts/ci/install-mathsat-macos-aarch64.sh
+++ scripts/ci/install-mathsat-macos-aarch64.sh
@@ -6,4 +6,4 @@ NAME=mathsat-5.6.8-osx
 TEMP=$(mktemp)
 curl -L -o "${TEMP}" "https://mathsat.fbk.eu/download.php?file=${NAME}.tar.gz"
 tar -zxvf "${TEMP}" "${NAME}/bin/mathsat"
-mv "${NAME}/bin/mathsat" /usr/local/bin
+sudo mv "${NAME}/bin/mathsat" /usr/local/bin
