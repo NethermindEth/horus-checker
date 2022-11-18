57c48a421ff99f6027cef4305b182ead98465224
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-11-18 13:12:23 -0500

    Use `brew` to install `z3` instead on macOS.

--- scripts/ci/install-z3-macos-aarch64.sh
+++ scripts/ci/install-z3-macos-aarch64.sh
@@ -5,8 +5,4 @@ set -eu
 version="4.10.2"
 arch="arm64-osx-11.0"
 
-curl -L -O https://github.com/Z3Prover/z3/releases/download/z3-$version/z3-$version-$arch.zip
-unzip z3-$version-$arch.zip
-sudo cp z3-$version-$arch/bin/libz3.so /usr/lib/libz3.so
-sudo cp z3-$version-$arch/include/* /usr/include/
-sudo cp z3-$version-$arch/bin/z3 /usr/bin/z3
+brew install z3@$version
