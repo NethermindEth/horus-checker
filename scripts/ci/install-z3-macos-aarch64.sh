a1635a9874a8f0e5d9b6299d543c8cd8cf087940
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-11-18 13:01:54 -0500

    Add `aarch64` SMT install scripts.

---
+++ scripts/ci/install-z3-macos-aarch64.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -eu
+
+version="4.10.2"
+arch="arm64-osx-11.0"
+
+sudo apt-get remove libz3-dev
+curl -L -O https://github.com/Z3Prover/z3/releases/download/z3-$version/z3-$version-$arch.zip
+unzip z3-$version-$arch.zip
+sudo cp z3-$version-$arch/bin/libz3.so /usr/lib/libz3.so
+sudo cp z3-$version-$arch/include/* /usr/include/
+sudo cp z3-$version-$arch/bin/z3 /usr/bin/z3
