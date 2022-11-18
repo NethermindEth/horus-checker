d5feead8d6127fe4f4acf7ae2c90334b67351aef
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-11-18 13:01:54 -0500

    Add `aarch64` SMT install scripts.

---
+++ scripts/ci/install-mathsat-macos-aarch64.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -eu
+
+NAME=mathsat-5.6.8-osx
+TEMP=$(mktemp)
+curl -L -o "${TEMP}" "https://mathsat.fbk.eu/download.php?file=${NAME}.tar.gz"
+tar -zxvf "${TEMP}" "${NAME}/bin/mathsat"
+mv "${NAME}/bin/mathsat" /usr/local/bin
