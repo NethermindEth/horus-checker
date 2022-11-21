d226c47d81bbdb0755e0e3a572432335a5a23e2e
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-11-21 13:54:54 -0500

    Remove truncation from `check.sh` script.
    * Update millisecond estimate for
      `all_three_unsigned_div_rem_preconditions_16000ms`.
    * Update `README` to reflect changes in `check.sh`.

--- tests/resources/golden/toy_amm/check.sh
+++ tests/resources/golden/toy_amm/check.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-DISPLAY_LINES=100
 
 # The first argument is the name of the `.cairo` program, and the second
 # argument is the per-module timeout in milliseconds.
-horus-compile $1 > out.json && time horus-check -s z3 --timeout $2 out.json | head -n $DISPLAY_LINES && rm out.json
-echo "Warning: only the first $DISPLAY_LINES lines printed."
-echo "You can adjust the 'DISPLAY_LINES' constant in the 'check.sh' script to show more output."
+horus-compile $1 > out.json && time horus-check -s z3 --timeout $2 out.json && rm out.json
