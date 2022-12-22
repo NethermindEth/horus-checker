818ca9584bface776a5719df20a0095a3ec6b774
Agustín Martinez Suñé <4691852+aemartinez@users.noreply.github.com>
2022-12-22 20:27:57 +0000

    Add `Dockerfile` for quick installation.
    * Remove temp directories created by install scripts.

--- scripts/ci/install-mathsat-macos.sh
+++ scripts/ci/install-mathsat-macos.sh
@@ -7,3 +7,4 @@ TEMP=$(mktemp)
 curl -L -o "${TEMP}" "https://mathsat.fbk.eu/download.php?file=${NAME}.tar.gz"
 tar -zxvf "${TEMP}" "${NAME}/bin/mathsat"
 sudo mv "${NAME}/bin/mathsat" /usr/local/bin
+rm -r "${NAME}" 
