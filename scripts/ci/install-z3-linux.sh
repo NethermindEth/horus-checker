dd6bb51d372d678d3ef6e750e22419ce9c8eee12
Agustín Martinez Suñé <4691852+aemartinez@users.noreply.github.com>
2022-12-22 20:27:57 +0000

    Add `Dockerfile` for quick installation.
    * Remove temp directories created by install scripts.

--- scripts/ci/install-z3-linux.sh
+++ scripts/ci/install-z3-linux.sh
@@ -5,8 +5,10 @@ set -eu
 version="4.10.2"
 
 sudo apt-get remove libz3-dev
-curl -L -O https://github.com/Z3Prover/z3/releases/download/z3-$version/z3-$version-x64-glibc-2.31.zip
-unzip z3-$version-x64-glibc-2.31.zip
+TEMP=$(mktemp)
+curl -L -o "${TEMP}" https://github.com/Z3Prover/z3/releases/download/z3-$version/z3-$version-x64-glibc-2.31.zip
+unzip "${TEMP}"
 sudo cp z3-$version-x64-glibc-2.31/bin/libz3.so /usr/lib/libz3.so
 sudo cp z3-$version-x64-glibc-2.31/include/* /usr/include/
 sudo cp z3-$version-x64-glibc-2.31/bin/z3 /usr/bin/z3
+rm -r z3-$version-x64-glibc-2.31
