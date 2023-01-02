62a97c2329e90810234ff5bcd25038ddb98054e4
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-01-02 11:22:03 -0500

    Add a hint about interpreting `Unknown` results.
    This commit adds a hint that gets printed when we have at least one
    `Unknown` in the output. The hint describes what `Unknown` means and how
    to increase the timeout.
    
    * Add `hie.yaml` for HLS.
    * Use `unless` to simplify printing of `Unknown` hint in `app/Main.hs`.
    * Add hint text to gold files with `Unknown`s.
    * Refactor tests file `bats-template` to reduce duplication.
    
    Since we now print hints, we must make the expected test output reflect
    that, fixing some minor issues with the test suite. In particular, we
    split the diffing in `bats-template` into a separate function.
    
    We also add a couple `sed` calls to strip lines starting with `hint:`
    from the Horus output, so that we don't get any false positives.
    
    The profiling functions are collapsed down into a single function called
    `single_solver_test_case` that takes an extra argument for the solver.
    
    * Modify CI profiling workflow accordingly.
    * Rename `entrypoint-profiling.sh` -> `profiling-entrypoint.sh`.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,15 @@
 module Main (main) where
 
 import Control.Applicative ((<**>))
+import Control.Monad (unless)
 import Control.Monad.Except (ExceptT, liftEither, runExceptT, throwError)
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson (FromJSON, eitherDecodeFileStrict)
 import Data.Foldable (for_)
 import Data.IORef (newIORef)
-import Data.Text (Text, pack, unpack)
-import Data.Text.IO qualified as Text (putStrLn)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.IO qualified as TextIO (putStrLn)
 import Lens.Micro ((%~), (<&>))
 import Options.Applicative
   ( execParser
@@ -22,12 +24,27 @@ import Options.Applicative.Help.Pretty (text)
 import Horus.Arguments (Arguments (..), argParser, fileArgument)
 import Horus.ContractDefinition (cdSpecs)
 import Horus.ContractInfo (mkContractInfo)
-import Horus.Global (SolvingInfo (..), solveContract)
+import Horus.Global (SolverResult (..), SolvingInfo (..), solveContract)
 import Horus.Global.Runner qualified as Global (Env (..), run)
 import Horus.SW.Std (stdSpecs)
 import Horus.Util (tShow)
 
 type EIO = ExceptT Text IO
+
+hint :: Text
+hint =
+  "A result of 'Unknown' indicates that Horus was not able to prove that the \n\
+  \specification holds *nor* find a counterxample. For functions yielding\n\
+  \'Unknown', try increasing the solver timeout by passing '-t <milliseconds>'.\n\
+  \The default is 2000ms.\n\
+  \\n\
+  \The timeout is per-SMT-query, which means that for a compiled contract\n\
+  \containing 7 functions, a 5000ms timeout implies a maximum running time of at\n\
+  \least 35 seconds. It can be more becuase a function sometimes requires multiple\n\
+  \SMT queries.\n\
+  \\n\
+  \Example:\n\
+  \  $ horus-check -s cvc5 -t 5000 a.json"
 
 main' :: Arguments -> EIO ()
 main' Arguments{..} = do
@@ -37,13 +54,17 @@ main' Arguments{..} = do
   let env = Global.Env{e_config = configRef, e_contractInfo = contractInfo}
   infos <- liftIO (Global.run env solveContract) >>= liftEither
   for_ infos $ \si -> liftIO $ do
-    Text.putStrLn (ppSolvingInfo si)
+    TextIO.putStrLn (ppSolvingInfo si)
+  let unknowns = [res | res@(Unknown{}) <- map si_result infos]
+  unless (null unknowns) $ liftIO (TextIO.putStrLn hint')
+ where
+  hint' = "\ESC[33m" <> (T.strip . T.unlines . map ("hint: " <>) . T.lines) hint <> "\ESC[0m"
 
 eioDecodeFileStrict :: FromJSON a => FilePath -> EIO a
 eioDecodeFileStrict path = do
   mbRes <- liftIO (eitherDecodeFileStrict path)
   case mbRes of
-    Left err -> throwError (pack err)
+    Left err -> throwError (T.pack err)
     Right res -> pure res
 
 ppSolvingInfo :: SolvingInfo -> Text
@@ -52,7 +73,7 @@ ppSolvingInfo SolvingInfo{..} = si_moduleName <> "\n" <> tShow si_result <> "\n"
 main :: IO ()
 main = do
   arguments <- execParser opts
-  runExceptT (main' arguments) >>= either (fail . unpack) pure
+  runExceptT (main' arguments) >>= either (fail . T.unpack) pure
  where
   opts =
     info
@@ -61,7 +82,7 @@ main = do
           <> progDescDoc
             ( Just $
                 text "Verifies "
-                  <> text (unpack fileArgument)
+                  <> text (T.unpack fileArgument)
                   <> text " (a contract compiled with horus-compile)\n\n"
                   <> text "Example using solver cvc5 (default):\n"
                   <> text "  $ horus-check a.json\n\n"
