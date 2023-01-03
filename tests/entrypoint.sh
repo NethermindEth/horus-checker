33f45035d0d933d122411799c6dba7863a49f1c8
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-01-03 15:56:04 -0500

    Install `cvc5` in CI, use it in tests, run on big machine.

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -5,5 +5,6 @@ set -eu
 RES_DIR="./tests/resources"
 bash "${RES_DIR}/generate-bats.sh" > "${RES_DIR}/tests.bats"
 jobs=$(getconf _NPROCESSORS_ONLN)
+printf "Num processors: $jobs\n"
 bats --jobs "${jobs}" "${RES_DIR}/tests.bats"
 rm "${RES_DIR}/tests.bats"
