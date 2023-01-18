fc84ddcb4484dd5eeeb3c404bab481b8a65a81c1
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-01-17 20:48:36 -0500

    Refactor handling of the `--version` flag
    This commit minimizes the diff, removes `guardVersion` and `if'`, and
    adds docstrings to some entrypoint functions.

--- src/Horus/Util.hs
+++ src/Horus/Util.hs
@@ -13,7 +13,6 @@ module Horus.Util
   , invert
   , eqT'
   , atMay
-  , if'
   )
 where
 
@@ -84,7 +83,3 @@ atMay (x : xs) n
   | n < 0 = Nothing
   | n == 0 = Just x
   | otherwise = atMay xs $ n - 1
-
-if' :: Bool -> a -> a -> a
-if' True x _ = x
-if' False _ y = y
