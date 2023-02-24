c14c84ac522e97cae278c97533fa0ba218e720ee
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-02-24 08:57:34 -0500

    dirty: Remove some tracing code

--- src/Horus/SW/Std.hs
+++ src/Horus/SW/Std.hs
@@ -13,11 +13,6 @@ import Horus.SW.FuncSpec (FuncSpec (..), emptyFuncSpec)
 import Horus.SW.ScopedName (ScopedName)
 import Horus.Util (tShow)
 
-import Debug.Trace (trace)
-
-traceAs :: Show a => String -> a -> a
-traceAs s x = trace (s ++ ": " ++ show x) x
-
 stdSpecs :: Map ScopedName FuncSpec
 stdSpecs = Map.fromList stdSpecsList
 
@@ -26,7 +21,7 @@ mkReadSpec name arity = emptyFuncSpec{fs_post = memory (ap - 1) .== var}
  where
   offsets = [-3 - arity + 1 .. -3]
   args = [memory (fp + fromIntegral offset) | offset <- offsets]
-  var = Expr.apply (Expr.Fun (tShow (traceAs "svar read name" name))) args
+  var = Expr.apply (Expr.Fun (tShow name)) args
 
 mkWriteSpec :: ScopedName -> Int -> FuncSpec
 mkWriteSpec name arity = emptyFuncSpec{fs_storage = [(name, [(args, memory (fp - 3))])]}
